1fba4d1bcbb2a2f5b9c46b84e99eaada7fcc3a27
longtk26 <longtk26@gmail.com>
2022-12-27 20:36:00 +0700

    set up

---
+++ src/components/EmojiResults/EmojiResults.js
@@ -0,0 +1,7 @@
+import "./EmojiResults.css";
+
+const EmojiResults = () => {
+  return <div>EmojiResults</div>;
+};
+
+export default EmojiResults;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <img
+        src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
+        alt="icon-1"
+      />
+      <p>Emoji Search</p>
+      <img
+        src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
+        alt="icon-2"
+      />
+    </header>
+  );
+};
+
+export default Header;
